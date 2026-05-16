5d680b226660dfa4806ef78cfa1a5820433c303a
Hellsingo-dev <murilo.martins10@fatec.sp.gov.br>
2026-05-15 22:34:54 -0300

    Add files via upload

---
+++ engenharia de software/Area-trianguloOO/src/entidades/Triangulo.java
@@ -0,0 +1,13 @@
+package entidades;
+
+public class Triangulo {
+    public double a;
+    public double b;
+    public double c;
+
+    // Método para calcular a área do triângulo
+    public double calcularArea() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
